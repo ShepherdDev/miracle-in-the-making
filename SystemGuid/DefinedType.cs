9526e8f17afb4c37631eb38cb1b03554f73c3cee
Daniel Hazelbaker <daniel@blueboxmoon.com>
2016-10-21 17:22:09 -0700

    Remove unnecessary defines.

--- SystemGuid/DefinedType.cs
+++ SystemGuid/DefinedType.cs
@@ -7,24 +7,5 @@ namespace com.shepherdchurch.MiracleInTheMaking.SystemGuid
     /// </summary>
     public static class DefinedType
     {
-        /// <summary>
-        /// Type's of Seat
-        /// </summary>
-        public const string SEAT_TYPE = "9545822B-5C5A-4AA7-B29A-44D218AF9351";
-
-        /// <summary>
-        /// Type's of Salvation
-        /// </summary>
-        public const string SALVATION_TYPE = "FDDA1641-4883-480A-863E-714E2A24CAE3";
-
-        /// <summary>
-        /// Type's of SeatPledge
-        /// </summary>
-        public const string SEATPLEDGE_TYPE = "FEE7745D-FF02-4296-913A-E80DD391F427";
-
-        /// <summary>
-        /// Type's of Dedication
-        /// </summary>
-        public const string DEDICATION_TYPE = "7B005F86-597A-4C7C-B8BA-155B54A4DEBB";
     }
 }
