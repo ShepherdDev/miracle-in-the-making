ca999b17a4ee082075cde417fa8dc7f396928970
Daniel Hazelbaker <daniel@blueboxmoon.com>
2016-10-31 15:08:21 -0700

    Add new MySeatPledges block and supporting CSS changes and chair image.

---
+++ Controls/MySeatPledges.ascx
@@ -0,0 +1,28 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="MySeatPledges.ascx.cs" Inherits="RockWeb.Plugins.com_shepherdchurch.MiracleInTheMaking.MySeatPledges" %>
+
+<asp:UpdatePanel ID="upnlContent" runat="server">
+    <ContentTemplate>
+        <Rock:NotificationBox ID="nbWarningMessage" runat="server" NotificationBoxType="Warning" />
+        <Rock:NotificationBox ID="nbInfoMessage" runat="server" NotificationBoxType="Info" />
+
+        <asp:Panel ID="pnlSeatPledges" runat="server" Visible="false" CssClass="mitm">
+            <div class="panel panel-default list-as-blocks clearfix">
+                <div class="panel-body">
+                    <ul>
+                        <asp:Repeater ID="rpSeatPledges" runat="server">
+                            <ItemTemplate>
+                                <li>
+                                    <a href="/page/<%= PageParameter( "SeatPledgeStatusPage" ) %>?seatPledgeId=<%# Eval( "Id" ) %>">
+                                        <i><asp:Image ID="imgChair" runat="server" ImageUrl="~/Plugins/com_shepherdchurch/MiracleInTheMaking/Assets/mitm_chair-128.png" /></i>
+                                        <h3><%# Eval( "AssignedSeat" ) != null ? Eval( "AssignedSeat.FriendlyName" ) : Eval( "RequestedSeat.FriendlyName" ) %></h3>
+                                        <%# Eval( "AssignedSeat" ) == null ? "<h2>(requested)</h2>" : "" %>
+                                    </a>
+                                </li>
+                            </ItemTemplate>
+                        </asp:Repeater>
+                    </ul>
+                </div>
+            </div>
+        </asp:Panel>
+    </ContentTemplate>
+</asp:UpdatePanel>
---
+++ Controls/MySeatPledges.ascx.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using com.shepherdchurch.MiracleInTheMaking.Data;
+using com.shepherdchurch.MiracleInTheMaking.Model;
+using Rock;
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.Cache;
+using Rock.Web.UI.Controls;
+
+namespace RockWeb.Plugins.com_shepherdchurch.MiracleInTheMaking
+{
+    [DisplayName( "My Seat Pledges" )]
+    [Category( "com_shepherdchurch > Miracle In The Making" )]
+    [Description( "Lists existing seat pledges for the currently logged in user." )]
+    [LinkedPage( "Seat Pledge Status Page", "Page to direct the user to when they select one of the seats from the list." )]
+
+    public partial class MySeatPledges : Rock.Web.UI.RockBlock
+    {
+        int _currentPersonId = 0;
+
+        #region Base Control Methods
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Init" /> event.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+
+            RockPage.AddCSSLink( ResolveUrl( "~/Plugins/com_shepherdchurch/MiracleInTheMaking/Styles/mitm.css" ) );
+
+            // this event gets fired after block settings are updated. it's nice to repaint the screen if these settings would alter it
+            this.BlockUpdated += Block_BlockUpdated;
+            this.AddConfigurationUpdateTrigger( upnlContent );
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Load" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad( e );
+
+            if ( !Page.IsPostBack )
+            {
+                if ( UserCanAdministrate && !(string.IsNullOrEmpty( PageParameter( "personId" ) )) )
+                {
+                    _currentPersonId = PageParameter( "personId" ).AsInteger();
+                }
+                else if ( CurrentPerson != null )
+                {
+                    _currentPersonId = CurrentPerson.Id;
+                }
+
+                if ( _currentPersonId == 0 )
+                {
+                    nbWarningMessage.Text = "You must be logged in to view your seat pledges.";
+                    nbWarningMessage.Visible = true;
+
+                    return;
+                }
+
+                pnlSeatPledges.Visible = true;
+                BindRepeater();
+            }
+        }
+
+        #endregion
+
+        #region Events
+
+        /// <summary>
+        /// Handles the BlockUpdated event of the control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void Block_BlockUpdated( object sender, EventArgs e )
+        {
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Binds the repeater.
+        /// </summary>
+        private void BindRepeater()
+        {
+            var service = new SeatPledgeService( new MiracleInTheMakingContext() );
+
+            var qry = service.Queryable().Where( sp => sp.PledgedPersonAlias.PersonId == _currentPersonId );
+
+            // Sort results
+            qry = qry.OrderBy( sp => sp.AssignedSeat != null ? sp.AssignedSeat.Section : sp.RequestedSeat.Section )
+                     .ThenBy( sp => sp.AssignedSeat != null ? sp.AssignedSeat.SeatNumber : sp.RequestedSeat.SeatNumber );
+            var results = qry.ToList();
+
+            rpSeatPledges.DataSource = results;
+            rpSeatPledges.DataBind();
+
+            if ( results.Count == 0 )
+            {
+                nbInfoMessage.Text = "You do not appear to currently have any seat pledges.";
+                nbInfoMessage.Visible = true;
+            }
+
+            if ( results.Count == 1 )
+            {
+                Dictionary<string, string> query = new Dictionary<string, string>();
+
+                query.Add( "seatPledgeId", results[0].Id.ToString() );
+                NavigateToLinkedPage( "SeatPledgeStatusPage", query );
+            }
+        }
+
+        /// <summary>
+        /// Navigates to detail page.
+        /// </summary>
+        /// <param name="seatPledgeId">The seat pledge identifier.</param>
+        private void NavigateToDetailPage( int seatPledgeId )
+        {
+            var qryParams = new Dictionary<string, string>();
+            qryParams.Add( "seatPledgeId", seatPledgeId.ToString() );
+            NavigateToLinkedPage( "PledgeDetailPage", qryParams );
+        }
+
+        /// <summary>
+        /// Navigates to detail page.
+        /// </summary>
+        /// <param name="dedicationId">The seat pledge identifier.</param>
+        private void NavigateToDedicationDetailPage( int dedicationId )
+        {
+            var qryParams = new Dictionary<string, string>();
+            qryParams.Add( "seatPledgeId", dedicationId.ToString() );
+            NavigateToLinkedPage( "DedicationDetailPage", qryParams );
+        }
+
+        #endregion
+    }
+}
